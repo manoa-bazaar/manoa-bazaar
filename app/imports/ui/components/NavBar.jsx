0f289e82fdb7e667ab072518a0f5bac8938cdc01
Lana Kawauchi <lanakawa@hawaii.edu>
2020-11-17 11:40:25 -1000

    Fixed ESLint Errors

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -18,8 +18,8 @@ class NavBar extends React.Component {
         {this.props.currentUser ? (
             [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Listing</Menu.Item>,
               <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Your Listings</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/categories" key='categories'>Categories</Menu.Item>]
-            [<Menu.Item as={NavLink} activeClassName="active" exact to="/user-profile" key='user-profile'>User Profile</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/categories" key='categories'>Categories</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/user-profile" key='user-profile'>User Profile</Menu.Item>,
               <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Your Listings</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
