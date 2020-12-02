364cf3c873e2fe3d0673fad3a6dc9636bf470c35
Lana Kawauchi <lanakawa@hawaii.edu>
2020-12-01 22:57:26 -1000

    Implement Feed

--- app/imports/ui/pages/ViewItem.jsx
+++ app/imports/ui/pages/ViewItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image, Divider, Button, Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Grid, Image, Divider, Button, Container, Header, Loader, Feed } from 'semantic-ui-react';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
@@ -54,6 +54,17 @@ class ViewItem extends React.Component {
                 <p>Listed 4 days ago in Kaneohe</p>
                 <Image src='images/map-placeholder.png' size='medium' rounded/>
               </Container>
+              <Feed>
+                <Feed.Event>
+                  <Image src='images/cat-icon.jpg' avatar/>
+                  <Feed.Content>
+                    <Feed.Date>3 days ago</Feed.Date>
+                    <Feed.Summary>
+                      <a>KittyJewel7981</a> has bid <a>$30</a>
+                    </Feed.Summary>
+                  </Feed.Content>
+                </Feed.Event>
+              </Feed>
             </Grid.Column>
           </Grid.Column>
         </Grid>
