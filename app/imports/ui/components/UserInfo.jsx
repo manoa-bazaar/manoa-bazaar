87d84ac64be8e0e77a3682d1ea5cd1cd001beade
Lana Kawauchi <lanakawa@hawaii.edu>
2020-12-01 22:49:54 -1000

    Related items will now appear in user profile

--- app/imports/ui/components/UserInfo.jsx
+++ app/imports/ui/components/UserInfo.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Grid, Rating, Image, Divider, Loader } from 'semantic-ui-react';
+import {Grid, Rating, Image, Divider, Loader, Card } from 'semantic-ui-react';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Users } from '../../api/users/User';
+import { Listings } from '../../api/listing/Listing';
+import ListingItem from '../components/ListingItem';
 
 /** A simple static component to render some text for the landing page. */
 class UserInfo extends React.Component {
@@ -28,42 +30,15 @@ class UserInfo extends React.Component {
               <p>Located in: {this.props.users.location}</p>
               <h4>80 items listed | 78 sales</h4>
               {this.props.users.description}
-          </Grid.Column>
+            </Grid.Column>
           </Grid.Row>
           <Divider fitted/>
           <Grid.Row columns={4}>
-            <Grid.Column size={1}>
-              <Image id="img-size" src='images/item1.jpg'/>
-              Louis Vuitton Palermo Monogram GM Brown
-              <h4>$1050</h4>
-            </Grid.Column>
-            <Grid.Column>
-              <Image id="img-size" src='images/item2.jpg'/>
-              Columbia South Canyon Diaper Bag Backpack
-              <h4>$13</h4>
-            </Grid.Column>
-            <Grid.Column>
-              <Image id="img-size" src='images/item3.jpg'/>
-              Blair Ritchey Penn Carryall in Chestnut
-              <h4>$320</h4>
-            </Grid.Column>
-            <Grid.Column>
-              <Image id="img-size" fluid src='images/item4.jpg'/>
-              Pokemon action figure lot
-              <h4>$10</h4>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns={4}>
-            <Grid.Column size={1}>
-              <Image id="img-size" src='images/item5.jpg'/>
-              Hello Kitty popcorn tokidoki figure
-              <h4>$5</h4>
-            </Grid.Column>
-            <Grid.Column>
-              <Image id="img-size" src='images/item6.jpg'/>
-              Tokidoki turtle
-              <h4>$5</h4>
-            </Grid.Column>
+            <Card.Group>
+              {this.props.list.map((item, index) => <ListingItem key={index}
+                                                                 item={item}
+              />)}
+            </Card.Group>
           </Grid.Row>
         </Grid>
     );
@@ -71,15 +46,18 @@ class UserInfo extends React.Component {
 }
 
 UserInfo.propTypes = {
-  users: PropTypes.array,
+  list: PropTypes.array.isRequired,
+  users: PropTypes.object,
   ready: PropTypes.bool.isRequired,
 };
 
 export default withTracker(() => {
   // Get access to Users documents.
   const subscription = Meteor.subscribe(Users.userPublicationName);
+  const subscription2 = Meteor.subscribe(Listings.itemPublicationName);
   return {
     users: Users.collection.findOne(),
-    ready: subscription.ready(),
+    list: Listings.collection.find({ category: 'Textbooks' }).fetch(),
+    ready: subscription.ready() && subscription2.ready(),
   };
 })(UserInfo);
