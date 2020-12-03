b274fa49316200196b61fb8f08aa0bd260c10a8e
DanielKimster <70424200+DanielKimster@users.noreply.github.com>
2020-12-03 11:12:56 -1000

    re-removed Stuffs publications

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,19 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/Stuff';
 import { Listings } from '../../api/listing/Listing';
 import { Bids } from '../../api/bids/Bids';
 import { Users } from '../../api/users/User';
-
-// User-level publication.
-// If logged in, then publish documents owned by this user. Otherwise publish nothing.
-Meteor.publish(Stuffs.userPublicationName, function () {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.collection.find({ owner: username });
-  }
-  return this.ready();
-});
 
 Meteor.publish(Listings.itemPublicationName, function () {
   if (this.userId) {
@@ -41,15 +30,6 @@ Meteor.publish(Users.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return Users.collection.find({ owner: username });
-  }
-  return this.ready();
-});
-
-// Admin-level publication.
-// If logged in and with admin role, then publish all documents from all users. Otherwise publish nothing.
-Meteor.publish(Stuffs.adminPublicationName, function () {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.collection.find();
   }
   return this.ready();
 });
