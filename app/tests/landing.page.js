0419e1167d748fd118b5b2492a2958aeec213ab0
DanielKimster <70424200+DanielKimster@users.noreply.github.com>
2020-12-15 13:45:19 -1000

    Update landing.page.js

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -8,7 +8,7 @@ class LandingPage {
 
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
-    await testController.wait(30000).expect(this.pageSelector.exists).ok();
+    await testController.wait(60000).expect(this.pageSelector.exists).ok();
   }
 }
 
